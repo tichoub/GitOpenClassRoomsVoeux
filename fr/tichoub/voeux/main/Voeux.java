2d7c3225ba7d4812d1a43410079ef17126387781
tichoub <tichoub@hotmail.fr>
2019-01-10 20:16:08 +0100

    Ajout au texte java d'excellente santé

--- fr/tichoub/voeux/main/Voeux.java
+++ fr/tichoub/voeux/main/Voeux.java
@@ -3,7 +3,7 @@ package fr.tichoub.voeux.main;
 public class Voeux {
 
 	public static void main(String[] args) {
-		System.out.println("Une belle année 2019 à  tous");
+		System.out.println("Une belle année 2019 à  tous et une excellente santé!");
 	}
 
 }
